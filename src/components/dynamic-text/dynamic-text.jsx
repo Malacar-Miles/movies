13d36f84ca50ccc59b45f0d71e813ea6e7b85482
Malacar-Miles <105152910+Malacar-Miles@users.noreply.github.com>
2022-08-24 05:25:52 +0300

    Implemented the Typing Text gimmick (not my work, found it on the internet).

--- src/components/dynamic-text/dynamic-text.jsx
+++ src/components/dynamic-text/dynamic-text.jsx
@@ -1,13 +1,19 @@
 // This is the text line on the homepage that changes dynamically
 // ("with X subtitles")
-// NOT IMPLEMENTED YET
 
 import "./dynamic-text.scss";
 
+import { useTypingText } from "../../hooks/typing-text";
+import { mapLanguageToCode } from "../../utils/menu-logic/language-codes";
+
+const languages = Object.keys(mapLanguageToCode);
+
 const DynamicText = () => {
+  const { word } = useTypingText(languages, 100, 10);
+
   return (
     <span className="dynamic-text-container">
-      with <span className="dynamic-language-string"></span> Subtitles
+      with <span className="dynamic-language-string">{word}</span>&nbsp;&nbsp;Subtitles
     </span>
   );
 };
